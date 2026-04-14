ebf4a3b550060cbf25cea12898193ffac7b93b06
Farhan Rasyid Mustaqim <farhanID0105@gmail.com>
2026-04-15 04:44:44 +0700

    simplified datahelper to help connect to the database

---
+++ SistemServisMotor/DatabaseHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemServisMotor
+{
+    public static class DatabaseHelper
+    {
+        public static string ConnectionString { get; } = "Data Source=siptea-lt\\FAR;Initial Catalog=DBBengkel;Integrated Security=True";
+
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(ConnectionString);
+        }
+
+        public static bool TestConnection()
+        {
+            try
+            {
+                using (SqlConnection conn = GetConnection())
+                {
+                    conn.Open();
+                    conn.Close();
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
