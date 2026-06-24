fcdf4bbdf80e44e031e661cf31d17cd319333d4e
Farhan Rasyid Mustaqim <farhanID0105@gmail.com>
2026-06-24 12:51:19 +0700

    Add dummy data, improve SP validation, and add print feature
    - Insert dummy data for Pelanggan, Kendaraan, Users, and Servis tables
    - Remove unused "GetAll" stored procedures for main entities
    - Enhance validation in Pelanggan and User SPs to prevent duplicates
    - Add transaction handling and validation to Kendaraan SPs
    - Introduce sp_PrintServis for printing service records by vehicle
    - Add check constraint for plat_no length in Kendaraan table

--- Query Bengkel Fixed(2).sql
+++ Query Bengkel Fixed(2).sql
@@ -94,8 +94,75 @@ CREATE TABLE Servis(
 );
 GO
 
+/*
+-- Data Dummy
+-- =============================================
+-- DATA DUMMY PELANGGAN (10 data)
+-- =============================================
+INSERT INTO Pelanggan (nama, alamat, no_hp) VALUES
+('Budi Santoso',      'Jl. Magelang No. 12, Yogyakarta',      '081234567890'),
+('Siti Rahayu',       'Jl. Kaliurang Km. 5, Sleman',          '082345678901'),
+('Ahmad Fauzi',       'Jl. Solo No. 45, Klaten',              '083456789012'),
+('Dewi Lestari',      'Jl. Parangtritis No. 7, Bantul',       '085678901234'),
+('Eko Prasetyo',      'Jl. Godean No. 23, Yogyakarta',        '087890123456'),
+('Fitria Handayani',  'Jl. Wates Km. 3, Kulon Progo',         '088901234567'),
+('Gunawan Hadi',      'Jl. Imogiri No. 88, Bantul',           '081122334455'),
+('Hani Permatasari',  'Jl. Ringroad Utara No. 15, Sleman',    '082233445566'),
+('Irwan Kusuma',      'Jl. Wonosari No. 30, Gunungkidul',     '083344556677'),
+('Joko Widiatmoko',   'Jl. Monjali No. 5, Yogyakarta',        '085566778899');
+GO
 
+-- =============================================
+-- DATA DUMMY KENDARAAN (13 data)
+-- =============================================
+INSERT INTO Kendaraan (id_pelanggan, merk, plat_no, tahun) VALUES
+(1,  'Honda Beat',        'AB 1234 AA', 2019),
+(1,  'Toyota Avanza',     'AB 5678 BB', 2021),  -- Budi punya 2 kendaraan
+(2,  'Yamaha NMAX',       'AB 2345 CC', 2020),
+(3,  'Honda Vario 125',   'AD 3456 DD', 2018),
+(4,  'Suzuki Ertiga',     'AB 4567 EE', 2022),
+(5,  'Mitsubishi Xpander','AB 5678 FF', 2021),
+(6,  'Honda Scoopy',      'AB 6789 GG', 2020),
+(7,  'Yamaha Mio M3',     'AB 7890 HH', 2017),
+(7,  'Daihatsu Xenia',    'AB 7891 HH', 2019),  -- Gunawan punya 2 kendaraan
+(8,  'Honda Jazz',        'AB 8901 II', 2018),
+(9,  'Toyota Kijang',     'AB 9012 JJ', 2015),
+(10, 'Kawasaki KLX',      'AB 0123 KK', 2022),
+(10, 'Honda Mobilio',     'AB 0124 KK', 2020);  -- Joko punya 2 kendaraan
+GO
 
+-- =============================================
+-- DATA DUMMY USERS (5 data)
+-- =============================================
+INSERT INTO Users (nama, username, no_telp, role) VALUES
+('Rizky Adriansyah',  'rizky.admin',    '081111222233', 'admin'),
+('Slamet Riyadi',     'slamet.petugas', '082222333344', 'petugas'),
+('Nurul Hidayah',     'nurul.petugas',  '083333444455', 'petugas'),
+('Dani Firmansyah',   'dani.admin',     '084444555566', 'admin'),
+('Wahyu Setiawan',    'wahyu.petugas',  '085555666677', 'petugas');
+GO
+
+-- =============================================
+-- DATA DUMMY SERVIS (15 data)
+-- =============================================
+INSERT INTO Servis (id_kendaraan, id_user, Tanggal, JenisServis, SukuCadang, Biaya, Catatan) VALUES
+(1,  2, '2024-01-05', 'Ganti Oli',            'Oli Mesin Shell 10W-40',          75000,  'Ganti oli rutin 3 bulan'),
+(2,  3, '2024-01-10', 'Tune Up',              'Busi NGK, Filter Udara',           150000, 'Mesin terasa berat saat start'),
+(3,  2, '2024-01-15', 'Ganti Ban',            'Ban Michelin 90/80-14',            350000, 'Ban belakang sudah botak'),
+(4,  5, '2024-02-01', 'Servis Rem',           'Kampas Rem Depan Belakang',        120000, 'Rem bunyi saat ditekan'),
+(5,  3, '2024-02-10', 'Ganti Oli + Tune Up',  'Oli Mobil 5W-30, Busi Denso',     250000, 'Servis rutin 10.000 km'),
+(6,  2, '2024-02-20', 'Ganti Aki',            'Aki Yuasa 12V 5Ah',               350000, 'Motor susah dinyalakan'),
+(7,  5, '2024-03-05', 'Ganti Oli',            'Oli Mesin Pertamina 10W-40',       65000,  'Ganti oli rutin'),
+(8,  3, '2024-03-12', 'Servis AC',            'Freon AC R134a, Filter Kabin',     450000, 'AC tidak dingin'),
+(9,  2, '2024-03-20', 'Tune Up',              'Filter Udara, Busi, Throttle Body',200000, 'Bensin boros'),
+(10, 5, '2024-04-01', 'Ganti Kampas Kopling', 'Kampas Kopling Honda Jazz',        500000, 'Kopling selip saat akselerasi'),
+(11, 3, '2024-04-15', 'Ganti Timing Belt',    'Timing Belt Toyota',               750000, 'Sudah 80.000 km belum pernah ganti'),
+(12, 2, '2024-05-02', 'Ganti Oli Gardan',     'Oli Gardan Shell Advance 90',      85000,  'Servis rutin'),
+(13, 5, '2024-05-10', 'Servis Suspensi',      'Shock Absorber Depan KYB',         900000, 'Mobil oleng saat menikung'),
+(1,  3, '2024-05-20', 'Ganti Oli',            'Oli Mesin Shell 10W-40',           75000,  'Servis rutin kedua'),
+(4,  2, '2024-06-01', 'Ganti Ban',            'Ban IRC 80/90-14',                 280000, 'Ban depan kempes terus');
+GO
+*/
 
 -- =============================================
 -- STORED PROCEDURES FOR PELANGGAN
@@ -112,19 +179,6 @@ SELECT
 FROM Pelanggan;
 GO
 
--- SP Get Pelanggan
-CREATE PROCEDURE sp_GetAllPelanggan
-AS
-BEGIN
-    SELECT
-        id_pelanggan AS ID,
-        nama AS Nama,
-        alamat AS Alamat,
-        no_hp AS [No HP]
-    FROM vwPelanggan;
-END
-GO
-
 -- SP Insert Pelanggan
 CREATE PROCEDURE sp_InsertPelanggan
     @nama VARCHAR(100),
@@ -136,7 +190,13 @@ BEGIN
 
     IF EXISTS (SELECT 1 FROM Pelanggan WHERE no_hp = @no_hp)
     BEGIN
-        RAISERROR('No. HP sudah terdaftar!', 16,1);
+        RAISERROR('No. HP sudah terdaftar!', 16, 1);
+        RETURN;
+    END
+
+    IF EXISTS (SELECT 1 FROM Pelanggan WHERE nama = @nama AND alamat = @alamat)
+    BEGIN
+        RAISERROR('Pelanggan dengan nama dan alamat yang sama sudah terdaftar!', 16, 1);
         RETURN;
     END
 
@@ -158,6 +218,18 @@ BEGIN
     IF NOT EXISTS (SELECT * FROM Pelanggan WHERE id_pelanggan = @id)
     BEGIN
         RAISERROR('Pelanggan tidak ditemukan!', 16, 1);
+        RETURN;
+    END
+
+    IF EXISTS (SELECT 1 FROM Pelanggan WHERE no_hp = @no_hp)
+    BEGIN
+        RAISERROR('No. HP sudah terdaftar!', 16, 1);
+        RETURN;
+    END
+
+    IF EXISTS (SELECT 1 FROM Pelanggan WHERE nama = @nama AND alamat = @alamat)
+    BEGIN
+        RAISERROR('Pelanggan dengan nama dan alamat yang sama sudah terdaftar!', 16, 1);
         RETURN;
     END
 
@@ -211,7 +283,6 @@ GO
 -- =============================================
 -- STORED PROCEDURES FOR KENDARAAN
 -- =============================================
-
 -- View Kendaraan
 CREATE VIEW vwKendaraan
 AS
@@ -224,22 +295,7 @@ SELECT
 FROM Kendaraan k
 JOIN Pelanggan p
     ON k.id_pelanggan = p.id_pelanggan;
-GO 
-
--- View Get Kendaraan    
-CREATE PROCEDURE sp_GetAllKendaraan
-AS
-BEGIN
-    SELECT
-        id_kendaraan AS [ID Kendaraan],
-        nama AS Pelanggan,
-        merk AS Merk,
-        plat_no AS [Plat No],
-        tahun AS Tahun
-    FROM vwKendaraan;
-END
 GO
-
 -- SP Insert Kendaraan
 CREATE PROCEDURE sp_InsertKendaraan
     @id_pel INT,
@@ -249,24 +305,28 @@ CREATE PROCEDURE sp_InsertKendaraan
 AS
 BEGIN
     SET NOCOUNT ON;
-
     IF EXISTS (SELECT 1 FROM Kendaraan WHERE plat_no = @plat_no)
     BEGIN
         RAISERROR('Plat No. sudah terdaftar!', 16, 1);
         RETURN;
     END
-
     IF NOT EXISTS (SELECT 1 FROM Pelanggan WHERE id_pelanggan = @id_pel)
     BEGIN
         RAISERROR('Pelanggan tidak ditemukan!', 16,1);
         RETURN;
     END
-
-    INSERT INTO Kendaraan(id_pelanggan, merk, plat_no, tahun)
-    VALUES(@id_pel, @merk, @plat_no, @tahun);
+    BEGIN TRANSACTION;
+    BEGIN TRY
+        INSERT INTO Kendaraan(id_pelanggan, merk, plat_no, tahun)
+        VALUES(@id_pel, @merk, @plat_no, @tahun);
+        COMMIT TRANSACTION;
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION;
+        RAISERROR('Gagal menyimpan data kendaraan!', 16, 1);
+    END CATCH
 END
 GO
-
 -- SP Update Kendaraan
 CREATE PROCEDURE sp_UpdateKendaraan
     @id INT,
@@ -277,38 +337,59 @@ CREATE PROCEDURE sp_UpdateKendaraan
 AS
 BEGIN
     SET NOCOUNT ON;
-
+    IF EXISTS (SELECT 1 FROM Kendaraan WHERE plat_no = @plat_no AND id_kendaraan <> @id)
+    BEGIN
+        RAISERROR('Plat No. sudah terdaftar!', 16, 1);
+        RETURN;
+    END
     IF NOT EXISTS (SELECT 1 FROM Pelanggan WHERE id_pelanggan = @id_pel)
     BEGIN
         RAISERROR('Pelanggan tidak ditemukan!', 16,1);
         RETURN;
     END
-
-    UPDATE Kendaraan
-    SET
-        id_pelanggan = @id_pel,
-        merk = @merk,
-        plat_no = @plat_no,
-        tahun = @tahun
-    WHERE id_kendaraan = @id;
+    IF NOT EXISTS (SELECT 1 FROM Kendaraan WHERE id_kendaraan = @id)
+    BEGIN
+        RAISERROR('Kendaraan tidak ditemukan!', 16, 1);
+        RETURN;
+    END
+    BEGIN TRANSACTION;
+    BEGIN TRY
+        UPDATE Kendaraan
+        SET
+            id_pelanggan = @id_pel,
+            merk = @merk,
+            plat_no = @plat_no,
+            tahun = @tahun
+        WHERE id_kendaraan = @id;
+        COMMIT TRANSACTION;
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION;
+        RAISERROR('Gagal mengubah data kendaraan!', 16, 1);
+    END CATCH
 END
 GO
-
 -- SP Delete Kendaraan
 CREATE PROCEDURE sp_DeleteKendaraan
     @id INT
 AS
 BEGIN
     SET NOCOUNT ON;
-
     IF NOT EXISTS (SELECT 1 FROM Kendaraan WHERE id_kendaraan = @id)
     BEGIN
         RAISERROR('Kendaraan tidak ditemukan!', 16,1);
         RETURN;
     END
-
-    DELETE FROM Kendaraan
-    WHERE id_kendaraan = @id;
+    BEGIN TRANSACTION;
+    BEGIN TRY
+        DELETE FROM Kendaraan
+        WHERE id_kendaraan = @id;
+        COMMIT TRANSACTION;
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION;
+        RAISERROR('Gagal menghapus data kendaraan!', 16, 1);
+    END CATCH
 END
 GO
 
@@ -351,19 +432,6 @@ SELECT
 FROM Users;
 GO 
     
--- SP Get Users
-CREATE PROCEDURE sp_GetAllUsers
-AS
-BEGIN
-    SELECT
-        id_user AS ID,
-        nama AS Nama,
-        username AS Username,
-        no_telp AS [No Telp],
-        role AS Role
-    FROM vwUsers;
-END
-GO
 -- SP Insert User
 CREATE PROCEDURE sp_InsertUser
     @nama VARCHAR(100),
@@ -374,9 +442,9 @@ AS
 BEGIN
     SET NOCOUNT ON;
 
-    IF EXISTS (SELECT 1 FROM Users WHERE username = @user)
+    IF EXISTS (SELECT 1 FROM Users WHERE username = @user AND no_telp = @telp)
     BEGIN
-        RAISERROR('Username sudah digunakan!', 16,1);
+        RAISERROR('Username atau No. Telepon sudah digunakan!', 16,1);
         RETURN;
     END
 
@@ -399,6 +467,12 @@ BEGIN
     IF NOT EXISTS (SELECT 1 FROM Users WHERE id_user = @id)
     BEGIN
         RAISERROR('User tidak ditemukan!', 16,1);
+        RETURN;
+    END
+
+    IF EXISTS (SELECT 1 FROM Users WHERE username = @user AND no_telp = @telp)
+    BEGIN
+        RAISERROR('Username atau No. Telepon sudah digunakan!', 16,1);
         RETURN;
     END
 
@@ -475,23 +549,6 @@ JOIN Kendaraan k
 JOIN Users u
     ON s.id_user = u.id_user;
 GO 
-
--- SP Get Servis
-CREATE PROCEDURE sp_GetAllServis
-AS
-BEGIN
-    SELECT
-	    id_servis AS [ID Servis],
-	    plat_no AS [Plat No],
-		Nama AS Petugas,
-	    Tanggal AS Tanggal,
-	    JenisServis AS [Jenis Servis],
-	    SukuCadang AS [Suku Cadang],
-	    Biaya AS [Biaya],
-	    Catatan AS Catatan
-    FROM vwServis;
-END
-GO
 
 -- SP Insert Servis
 CREATE PROCEDURE sp_InsertServis
@@ -645,4 +702,41 @@ SELECT * INTO Kendaraan_Backup FROM Kendaraan;
 SELECT * INTO Users_Backup FROM Users;
 SELECT * INTO Servis_Backup FROM Servis;
 
-
+select * from Users-- Fitur PrintCREATE PROCEDURE sp_PrintServis
+    @id_kendaraan INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+    
+    SELECT
+        Users.nama AS Nama_User,
+        Users.role AS Role,
+        Kendaraan.merk AS Merk,
+        Kendaraan.tahun AS Tahun,
+        Pelanggan.nama AS Nama_Pelanggan,
+        Kendaraan.plat_no AS No_Plat,
+        Servis.Tanggal,
+        Servis.JenisServis AS Jenis_Servis,
+        Servis.SukuCadang AS Suku_Cadang,
+        Servis.Biaya,
+        Servis.Catatan
+    FROM
+        Servis
+    JOIN
+        Kendaraan ON Servis.id_kendaraan = Kendaraan.id_kendaraan
+    JOIN
+        Pelanggan ON Kendaraan.id_pelanggan = Pelanggan.id_pelanggan
+    JOIN
+        Users ON Servis.id_user = Users.id_user
+    WHERE 
+        Kendaraan.id_kendaraan = @id_kendaraan
+    ORDER BY 
+        Servis.Tanggal DESC;
+
+    IF @@ROWCOUNT = 0
+    BEGIN
+        RAISERROR('Tidak ada data servis!', 16, 1);
+    END
+END;EXEC sp_PrintServis;-- Alter validasi jumlah digit plat no untuk tabel kendaraanALTER TABLE Kendaraan
+ADD CONSTRAINT CK_Kendaraan_plat_no
+CHECK (LEN(plat_no) BETWEEN 1 AND 11);
