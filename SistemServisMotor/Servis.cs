8db9394e2848512013b770e048ebae33647bf086
Farhan Rasyid Mustaqim <farhanID0105@gmail.com>
2026-06-24 12:50:56 +0700

    Add Servis class with service-related properties
    Added Servis.cs defining the Servis class in the SistemServisMotor namespace. The class includes properties for user, role, motorcycle details, customer info, service date, type, spare parts, cost, and notes.

---
+++ SistemServisMotor/Servis.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemServisMotor
+{
+    internal class Servis
+    {
+        public string Nama_User { get; set; }
+        public string Role { get; set; }
+        public string Merk { get; set; }
+        public int Tahun { get; set; }
+        public string Nama_Pelanggan { get; set; }
+        public string No_Plat { get; set; }          
+        public DateTime Tanggal { get; set; }
+        public string Jenis_Servis { get; set; }     
+        public string Suku_Cadang { get; set; }      
+        public int Biaya { get; set; }
+        public string Catatan { get; set; }
+    }
+}
